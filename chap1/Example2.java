37d46ff468c14fc7ca29e491d5d142c3f57e33bc
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-09-04 11:00:57 -0500

    modify example 2

--- chap1/Example2.java
+++ chap1/Example2.java
@@ -6,10 +6,10 @@
 
 class Example2 {
 	public static void main(String args[]) {
-		int var1;
-		int var2;
+		int var1; // this declares a variable
+		int var2; // this declares another variable
 
-		var1 = 1024;
+		var1 = 1024; // this assigns 1024 to var1
 
 		System.out.println("var1 contains " + var1);
 
