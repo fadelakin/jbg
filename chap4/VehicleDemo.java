ebfaa039a2730ac08d6bb7abe075862768a4722b
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-09-12 21:53:16 -0400

    modify classes

--- chap4/VehicleDemo.java
+++ chap4/VehicleDemo.java
@@ -1,12 +1,7 @@
 /*  A program that uses the Vehicle class
 	
 	Call this  file VehicleDemo.java */
-class Vehicle {
-	int passengers; // number of passengers
-	int fuelcap;	// fuel capacity in gallons
-	int mpg;		// fuel consumption in miles per gallon
-}
-
+	
 // This class declares on object of type Vehicle
 class VehicleDemo {
 	public static void main(String args[]) {
