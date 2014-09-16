8aa001ff11478e15d374f637c366caaf9ffc0ecd
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-09-16 15:25:54 -0500

    Try This 4-1

---
+++ chap4/HelpClassDemo.java
@@ -0,0 +1,28 @@
+// Try This 4-1
+// 
+
+class HelpClassDemo {
+	public static void main(String args[]) throws java.io.IOException {
+		char choice, ignore;
+		Help hlpobj = new Help();
+
+		for(;;) {
+			do {
+				hlpobj.showMenu();
+
+				choice = (char) System.in.read();
+
+				do {
+					ignore = (char) System.in.read();
+				} while(ignore != '\n');
+
+			} while ( !hlpobj.isValid(choice) );
+
+			if(choice == 'q') break;
+
+			System.out.println("\n");
+
+			hlpobj.helpOn(choice);
+		}
+	}
+}
