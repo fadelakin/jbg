f18b438b650271259ad517851f10d71f1397afbe
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-09-23 14:53:58 -0500

    Demonstrate a one-dimensional array - fixed an error

--- chap5/ArrayDemo.java
+++ chap5/ArrayDemo.java
@@ -5,7 +5,7 @@ class ArrayDemo {
 		int i;
 
 		for(i = 0; i < 10; i = i+1)
-			sample[i] = 1;
+			sample[i] = i;
 
 		for(i = 0; i < 10;  i = i+1)
 			System.out.println("This is sample[" + i + "]: " + sample[i]);
