9ae2fcf7e24f63fe85f678a52b35fe5b37ec7c3f
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-10-20 15:13:12 -0400

    Demonstrate using command line arguments

---
+++ chap5/CLDemo.java
@@ -0,0 +1,12 @@
+// Display all command-line information
+// 
+// Run program: java CLDemo one two three
+class CLDemo {
+	public static void main(String args[]) {
+		System.out.println("There are " + args.length + " command-line arguments.");
+
+		System.out.println("They are: ");
+		for(int i = 0; i < args.length; i++)
+			System.out.println("arg[" + i + "]: " + args[i]);
+	}
+}
