a2657d8d7f80ea3163663fde06a160ba102e78c6
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-10-20 15:03:43 -0400

    Demonstrate using substring()

---
+++ chap5/SubStr.java
@@ -0,0 +1,12 @@
+// Demonstrate using substring()
+class SubStr {
+	public static void main(String args[]) {
+		String orgstr = "Java makes the web move.";
+
+		// construct a substring
+		String substr = orgstr.substring(5, 18);
+
+		System.out.println("orgstr: " + orgstr);
+		System.out.println("substr: " + substr);
+	}
+}
