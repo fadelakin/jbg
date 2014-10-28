6ecad112ff25ce490773500f7d5f397b53ccc4d2
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-10-28 16:31:28 -0400

    Demonstrate passing objects through their references

---
+++ chap6/PassObRef.java
@@ -0,0 +1,12 @@
+// Pass objects through their references.
+class PassObRef {
+	public static void main(String args[]) {
+		Test ob = new Test(15, 20);
+
+		System.out.println("ob.a and ob.b before call: " + ob.a + " " + ob.b);
+
+		ob.change(ob);
+
+		System.out.println("ob.a and ob.b after call: " + ob.a + " " + ob.b);
+	}
+}
