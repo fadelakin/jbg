f0d3fdea8864b0fd46348cd61fb9287d8ac69d1c
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-12 20:39:34 -0500

    Demonstrate creating a static method

--- chap6/SDemo2.java
+++ chap6/SDemo2.java
@@ -7,3 +7,15 @@ class StaticMeth {
 		return val/2;
 	}
 }
+
+class SDemo2 {
+	public static void main(String args[]) {
+
+		System.out.println("val is " + StaticMeth.val);
+		System.out.println("StaticMeth.valDiv2(): " + StaticMeth.valDiv2());
+
+		StaticMeth.val = 4;
+		System.out.println("val is " + StaticMeth.val);
+		System.out.println("StaticMeth.valDiv2(): " + StaticMeth.valDiv2());
+	}
+}
