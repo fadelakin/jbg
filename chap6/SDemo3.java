2aeb425669147472dc152a664f3eee1d1b1a676d
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-14 22:53:52 -0500

    write staticblock class

---
+++ chap6/SDemo3.java
@@ -0,0 +1,15 @@
+// Use a static block
+class StaticBlock {
+	static double rootOf2;
+	static double rootOf3;
+
+	static {
+		System.out.println("Inside a static block");
+		rootOf2 = Math.sqrt(2.0);
+		rootOf3 = Math.sqrt(3.0);
+	}
+
+	StaticBlock(String msg) {
+		System.out.println(msg);
+	}
+}
