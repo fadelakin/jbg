da089e5f1089f0ae6fa5eb78a352395394c23e08
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-02 20:10:13 -0500

    create TwoDShape class

---
+++ chap7/TwoDShape.java
@@ -0,0 +1,11 @@
+// A simple class hireachy
+// 
+// A class for two dimensional objects
+class TwoDShape {
+	double width;
+	double hieght;
+
+	void showDim() {
+		System.out.println("Width and height are " + width + " and " + height);
+	}
+}
