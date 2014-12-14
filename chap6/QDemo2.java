8724b5dcde5a44f1ec0d58615deb0b5cccf3314f
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-13 20:09:17 -0500

    add constructors

--- chap6/QDemo2.java
+++ chap6/QDemo2.java
@@ -3,9 +3,30 @@ class Queue {
 	private char q[]; // this array holds the queue
 	private int putloc, getloc; // the put and get indices
 
+	// Construct an empty Queue given its size
 	Queue(int size) {
 		q = new char[size]; // allocate memory for queue.
 		putloc = getloc = 0;
+	}
+
+	// Construct a Queue from a Queue
+	Queue(Queue ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+
+		// copy elements
+		for(int i = getloc; i < putloc; i++)
+			q[i] = ob.q[i];
+	}
+
+	// Construct a Queue with initial values.
+	Queue(char a[]) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+
+		for(int i = 0; i < a.length; i++) put(a[i]);
 	}
 
 	// put a character into the queue
