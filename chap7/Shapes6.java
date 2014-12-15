9892aaf6e673f19518154451198b091abddace67
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-14 21:31:49 -0500

    Extend

---
+++ chap7/Shapes6.java
@@ -0,0 +1,64 @@
+// A multilevel hierarchy
+class TwoDShape {
+	private double width; // these are
+	private double height; // now private
+
+	// A default constructor.
+	TwoDShape() {
+		width = height = 0.0;
+	}
+
+	// Parameterized constructor.
+	TwoDShape(double w, double h) {
+		width = w;
+		height = h;
+	}
+
+	// Construct ovject with equal width and height
+	TwoDShape(double x) {
+		width = height = x;
+	}
+
+	// Accessor methods for width and height
+	double getWidth() { return width; }
+	double getHeight() { return height; }
+	void setWidth(double w) { width = w; }
+	void setHeight(double h) { height = h; }
+
+	void showDim() {
+		System.out.println("Width and height are " + width + " and " + height);
+	}
+}
+
+// Extend TwoDShape.
+class Triangle extends TwoDShape {
+	private String style;
+
+	// A default constructor
+	Triangle() {
+		super();
+		style = "none";
+	}
+
+	// Add a constructor to Triangle
+	Triangle(String s, double w, double h) {
+		super(w, h); // call superclass constructor
+
+		style = s;
+	}
+
+	// One argument constructor
+	Triangle(double x) {
+		super(x); // call superclass constructor
+
+		style = "filled";
+	}
+
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+
+	void showStyle() {
+		System.out.println("Triangle is " + style);
+	}
+}
