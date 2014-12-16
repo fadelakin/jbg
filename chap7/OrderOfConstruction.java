abfa9f838d3290e3cc027d74d3719c3c2198caee
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-15 21:49:40 -0500

    write class B

--- chap7/OrderOfConstruction.java
+++ chap7/OrderOfConstruction.java
@@ -6,3 +6,10 @@ class A {
 		System.out.println("Constructing A.");
 	}
 }
+
+// Create a subclass by extending class A.
+class B extends A {
+	B() {
+		System.out.println("Constructing B.");
+	}
+}
