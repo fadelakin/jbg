96dd0b4943b48ab4c03c0f1754c0134f81aaa719
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-17 21:29:59 -0500

    write class b

--- chap7/Override.java
+++ chap7/Override.java
@@ -11,3 +11,17 @@ class A {
 		System.out.println("i and j: " + i + " " + j);
 	}
 }
+
+class B extends A {
+	int k;
+
+	B(int a, int b, int c) {
+		super(a, b);
+		k = c;
+	}
+
+	// display k - this overrides show() in A
+	void show() {
+		System.out.println("k: " + k);
+	}
+}
