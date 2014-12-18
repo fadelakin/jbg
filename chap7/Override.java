88d8fb75b9293636031a95fd22bf56db9063d78c
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-17 21:28:32 -0500

    Write class A

---
+++ chap7/Override.java
@@ -0,0 +1,13 @@
+// Method overriding
+class A {
+	int i, j;
+	A(int a, int b) {
+		i = a;
+		j = b;
+	}
+
+	// display i and j
+	void show() {
+		System.out.println("i and j: " + i + " " + j);
+	}
+}
