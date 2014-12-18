ebd7c78e21683f61d55a323c75a3461298490a80
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-17 21:39:13 -0500

    demonstrate method overloading

--- chap7/Overload.java
+++ chap7/Overload.java
@@ -27,3 +27,12 @@ class B extends A {
 		System.out.println(msg + k);
 	}
 }
+
+class Overload {
+	public static void main(String args[]) {
+		B subOb = new B(1, 2, 3);
+
+		subOb.show("This is k: "); // this calls show() in B
+		subOb.show(); // this class show() in A
+	}
+}
