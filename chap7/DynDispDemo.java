ac173df38ab165f3e2a6c85d1ad719e2bf7c6d6a
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-18 22:58:06 -0500

    Demonstrate dynamic method dispatch

--- chap7/DynDispDemo.java
+++ chap7/DynDispDemo.java
@@ -17,3 +17,22 @@ class Sub2 extends Sup {
 		System.out.println("who() in Sub2");
 	}
 }
+
+class DynDispDemo {
+	public static void main(String args[]) {
+		Sup superOb = new Sup();
+		Sub1 subOb1 = new Sub1();
+		Sub2 subOb2 = new Sub2();
+
+		Sup supRef;
+
+		supRef = superOb;
+		supRef.who();
+
+		supRef = subOb1;
+		supRef.who();
+
+		supRef = subOb2;
+		supRef.who();
+	}
+}
